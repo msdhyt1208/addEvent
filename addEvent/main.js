1a4710c5450e131790ecc48589ec1bd9c977568c
msdhyt1208 <msdhyt1208@gmail.com>
2023-02-16 01:12:33 +0900

    エラー　テキストドラッグ可能

--- addEvent/main.js
+++ addEvent/main.js
@@ -1,10 +1,17 @@
 const addBtn = document.querySelectorAll(".add>button");
 const removeBtn = document.querySelectorAll(".remove>button");
 const ul = document.querySelector("ul");
+
+
 const lineDrag = document.querySelector(".line-move");
 const lineMove = document.querySelector(".line-move span:nth-child(1)");
+let moveX;
 let drag = null;
 
+
+
+
+const mousePosition = document.querySelector(".mouse-move")
 
 let numbar = 0;
 
@@ -56,15 +63,48 @@ for(let i= 0;i<removeBtn.length;i++){
 }
 
 lineDrag.addEventListener("dragstart",function(event){
-  console.log("dragstart:"+event.rayerX);
-  
 })
 lineDrag.addEventListener("dragover",function(event){
   event.preventDefault();
-  console.log("dragover:"+event.clientX);
-  lineMove.style.width = event.clientX+"px";
+  lineMove.style.width = event.clientX-100 + "px";
 })
 lineDrag.addEventListener("drop",function(event){
-  console.log(event);
-  lineMove.style.width = event.clientX+"px";
+  lineMove.style.width = event.clientX-100 + "px";
+})
+
+// https://developer.mozilla.org/ja/docs/Web/API/HTMLElement/drag_event
+const blockBox = document.querySelectorAll(".block-move>ul");
+const block = document.querySelectorAll(".block-move>ul>li");
+blockSelect = null;
+console.log(blockBox.length);
+for(let i=0;i<blockBox.length;i++){
+
+  blockBox[i].addEventListener("dragstart",function(event){
+    blockSelect = event.target;
+
+    console.log(event)
+    // blockSelect = block[i];
+  }) 
+  blockBox[i].addEventListener("dragover",function(event){
+    event.preventDefault();
+  }) 
+  blockBox[i].addEventListener("drop",function(event){
+    event.preventDefault();
+    if(blockSelect !== null){
+      blockSelect.remove();
+      event.target.appendChild(blockSelect);
+      blockSelect =null;
+    }
+  })
+
+
+}
+
+
+
+
+mousePosition.addEventListener("mousemove",function(e){
+  console.log(e.clientX-e.offsetX);
+
+  this.firstElementChild.textContent = `clientX${e.clientX}: clientY${e.clientY}`;
 })
