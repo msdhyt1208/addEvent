e6bfa11700eda354d41dee39ffdc2a4e408d35d3
msdhyt1208 <msdhyt1208@gmail.com>
2023-02-16 12:42:40 +0900

    スクロール追加　図形移動時バグ修正

--- addEvent/main.js
+++ addEvent/main.js
@@ -1,26 +1,15 @@
 const addBtn = document.querySelectorAll(".add>button");
 const removeBtn = document.querySelectorAll(".remove>button");
 const ul = document.querySelector("ul");
-
-
-const lineDrag = document.querySelector(".line-move");
-const lineMove = document.querySelector(".line-move span:nth-child(1)");
-let moveX;
-let drag = null;
-
-
-
-
-const mousePosition = document.querySelector(".mouse-move")
-
 let numbar = 0;
-
+blockSelect = null;
 
 // <li>作成ボタン
 for(let i=0;i<addBtn.length;i++){
   addBtn[i].addEventListener("click",function(){
     const crLi = document.createElement("li");
     crLi.textContent = `${numbar++}`;
+    crLi.draggable = true;
     crLi.addEventListener("click",function(){
       const selc = document.querySelectorAll(".select");
       if(this.className !== ""){
@@ -29,6 +18,9 @@ for(let i=0;i<addBtn.length;i++){
       }
       if(selc.length !== 0)   selc[0].className = "";
       this.className = "select";
+    })
+    crLi.addEventListener("dragstart",function(event){
+      blockSelect = event.target;
     })
     ul.appendChild(crLi);
     const selct = document.querySelector(".select");
@@ -40,7 +32,7 @@ for(let i=0;i<addBtn.length;i++){
         ul.insertBefore(crLi,selct);
         break;
       case 2:
-        ul.insertBefore(crLi,selct.nextSibling);
+        ul.insertBefore(crLi,selct.nextElementSibling);
         break;
     }
   })
@@ -62,28 +54,37 @@ for(let i= 0;i<removeBtn.length;i++){
   })
 }
 
+
+
+const lineDrag = document.querySelector(".line-move");
+const lineMove = document.querySelectorAll(".line-move span");
+let lineselect = null;
 lineDrag.addEventListener("dragstart",function(event){
+  if(event.target === lineMove[1])  lineselect = true;
 })
 lineDrag.addEventListener("dragover",function(event){
-  event.preventDefault();
-  lineMove.style.width = event.clientX-100 + "px";
+  
+  if(lineselect){
+    lineMove[0].style.width = event.clientX-100 + "px";
+    event.preventDefault();
+  }
 })
 lineDrag.addEventListener("drop",function(event){
-  lineMove.style.width = event.clientX-100 + "px";
+  event.preventDefault();
+  if(lineselect)    lineselect = false;
 })
+
+
+
 
 // https://developer.mozilla.org/ja/docs/Web/API/HTMLElement/drag_event
 const blockBox = document.querySelectorAll(".block-move>ul");
 const block = document.querySelectorAll(".block-move>ul>li");
-blockSelect = null;
 console.log(blockBox.length);
 for(let i=0;i<blockBox.length;i++){
 
   blockBox[i].addEventListener("dragstart",function(event){
     blockSelect = event.target;
-
-    console.log(event)
-    // blockSelect = block[i];
   }) 
   blockBox[i].addEventListener("dragover",function(event){
     event.preventDefault();
@@ -92,19 +93,39 @@ for(let i=0;i<blockBox.length;i++){
     event.preventDefault();
     if(blockSelect !== null){
       blockSelect.remove();
-      event.target.appendChild(blockSelect);
+      this.appendChild(blockSelect);
       blockSelect =null;
     }
   })
-
-
+ 
+  
 }
 
 
 
 
+const mousePosition = document.querySelector(".mouse-move")
 mousePosition.addEventListener("mousemove",function(e){
-  console.log(e.clientX-e.offsetX);
 
   this.firstElementChild.textContent = `clientX${e.clientX}: clientY${e.clientY}`;
 })
+
+
+
+// const scrollBox =  document.querySelector(".scroll");
+// scrollBox.addEventListener("scroll", function(event){
+//   console.log(event);
+// });
+
+
+
+
+
+
+
+addEventListener('abort',       function(event) { console.log(`about`)});
+addEventListener('ended',       function()      { console.log(`ended`)});
+addEventListener('addtrack',    function(event) { console.log(`addtrack`)});
+addEventListener('change',      function(event) { console.log(`change`)});
+addEventListener('messageerror',function(event) { console.log(`messageerror`)});
+addEventListener('message"',    function(event) { console.log(`message"`)});
